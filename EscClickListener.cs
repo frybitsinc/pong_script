039cfc8a201c7e2682955d0cb24a225f7819fe13
frybits <frybitsofficial@gmail.com>
2017-07-04 22:11:20 +0900

    Added Esc click listener to quit game (on WallLeft).

---
+++ EscClickListener.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscClickListener : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+        // Click ESC
+        if (Input.GetKey(KeyCode.Escape)) {
+            // Quit game
+            Application.Quit();
+        }
+    }
+}
