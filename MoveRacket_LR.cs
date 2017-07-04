9c595f1e5b9bc83a0e377e8f9dc9b51961d7246d
frybits <frybitsofficial@gmail.com>
2017-07-04 21:04:30 +0900

    Basic Left, Right, Top, BottomRacket movement script done.

--- MoveRacket_LR.cs
+++ MoveRacket_LR.cs
@@ -7,6 +7,6 @@ public class MoveRacket_LR : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
         float v = Input.GetAxisRaw("Vertical");
-        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
 	}
 }
---
+++ MoveRacket_TB.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRacket_TB : MonoBehaviour {
+    public float speed = 30;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float v = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(v, 0) * speed;
+    }
+}
