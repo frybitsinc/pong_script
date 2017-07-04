f47a1f628ddb457439ebcbe5df150f6782ef7388
frybits <frybitsofficial@gmail.com>
2017-07-04 20:16:01 +0900

    first commit

---
+++ MoveRacket_LR.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRacket_LR : MonoBehaviour {
+    public float speed = 30;
+    
+	// Update is called once per frame
+	void FixedUpdate () {
+        float v = Input.GetAxisRaw("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v);
+	}
+}
