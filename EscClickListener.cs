b91f4071d125892bd4f493650dedd8271d64c18b
frybits <frybitsofficial@gmail.com>
2018-01-11 23:52:20 +0900

    add esc click listener

--- EscClickListener.cs
+++ EscClickListener.cs
@@ -10,5 +10,27 @@ public class EscClickListener : MonoBehaviour {
             // Quit game
             Application.Quit();
         }
+        if (Input.GetKey(KeyCode.Space)) {
+            if (!paused)
+            {
+                // Pause game
+                PauseGame();
+            }
+            else {
+                // Resume game
+                PauseGame();
+            }
+        }
+    }
+
+    private void PauseGame() {
+        Time.timeScale = 0;
+        //pausePanel.SetActive(true);
+        //Disable scripts that still work while timescale is set to 0
+    }
+    private void ResumeGame() {
+        Time.timeScale = 1;
+        //pausePanel.SetActive(false);
+        //enable the scripts again
     }
 }
