bca755e4ef15fad167f0b54635a7d0c35976454c
frybits <frybitsofficial@gmail.com>
2018-01-12 01:17:38 +0900

    edited esc click, pause/resume function

--- EscClickListener.cs
+++ EscClickListener.cs
@@ -1,36 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EscClickListener : MonoBehaviour {
-    
-	// Update is called once per frame
-	void Update () {
+public class EscClickListener : MonoBehaviour
+{
+    public Transform canvas;
+
+    void Start()
+    {
+        // hide pause menu on start
+        canvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         // Click ESC
-        if (Input.GetKey(KeyCode.Escape)) {
-            // Quit game
-            Application.Quit();
-        }
-        if (Input.GetKey(KeyCode.Space)) {
-            if (!paused)
-            {
-                // Pause game
-                PauseGame();
-            }
-            else {
-                // Resume game
-                PauseGame();
-            }
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Pause();
         }
     }
 
-    private void PauseGame() {
-        Time.timeScale = 0;
-        //pausePanel.SetActive(true);
-        //Disable scripts that still work while timescale is set to 0
-    }
-    private void ResumeGame() {
-        Time.timeScale = 1;
-        //pausePanel.SetActive(false);
-        //enable the scripts again
+    public void Pause()
+    {
+        if (canvas.gameObject.activeInHierarchy == false)
+        {
+            // Pause game
+            Time.timeScale = 0;
+            canvas.gameObject.SetActive(true);
+        }
+        else {
+            // Resume game
+            Time.timeScale = 1;
+            canvas.gameObject.SetActive(false);
+        }
     }
 }
